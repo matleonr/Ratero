8a09a4ceb10532f37585d1dfb74950e59ab8f40f
Eveling Jaramillo <evelingjaramillo19@icloud.com>
2023-09-28 10:36:32 -0500

    primeros pasos

---
+++ Ratero/RateroApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateroApp.swift
+//  Ratero
+//
+//  Created by Eveling Jaramillo on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RateroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
